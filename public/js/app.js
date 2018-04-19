fb0e4eea2b0df46d0655ebe986e037fa5b708726
ohrha <trigramthree@gmail.com>
2018-04-19 23:00:07 +1200

    ADD-TAP-TARGET-BEGIN-HEROKU-TEST

---
+++ public/js/app.js
@@ -0,0 +1,16 @@
+(function(){
+
+//console.log("app.js loaded");
+
+var app = angular.module('qlc',["tapTargetController"]);
+																																																  
+app.config(function(){
+
+	//$httpProvider.interceptors.push('AuthInterceptors');
+	//$window.Stripe.setPublishableKey('pk_test_aE3UDuxFXzcslBrNanFIIi6Q');
+    console.log("app module loaded.")
+
+});
+}());
+//app.controller('StoreController', [  function() {
+
