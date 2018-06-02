6c803f33f99dc7dfbbdef969387c0648de02a0c9
ohrha <trigramthree@gmail.com>
2018-06-03 03:51:55 +1200

    WORK-ON-DELINQUENT-TIMESHEET-FUNCTIONALITY-AND-DATABASE-ENTRY

--- public/controllers/managementCtrl.js
+++ public/controllers/managementCtrl.js
@@ -893,7 +893,6 @@
                                     $scope.jobsSelected = false;
                                     $scope.complaintsSelected = false;
                                     $scope.delinquentTimeSheetPageOpened = true;
-                                    $scope.userFilePage = false;
                                     $scope.userDetailsPageOpened = false;
                                     $scope.bookedJobsPageOpened = false;
                                     $scope.complaintsPageOpened = false;
