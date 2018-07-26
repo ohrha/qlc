7727406a6e1b31bb02ca37317bfb423134bdd87b
ohrha <trigramthree@gmail.com>
2018-07-26 19:29:55 +1200

    Display Page

---
+++ public/controllers/displayCtrl.js
@@ -0,0 +1,139 @@
+(function () {
+
+    var app = angular.module('displayController', ['userServices'])
+    app.config(function () {
+
+        console.log("display Controller Loaded")
+    })
+
+    app.controller('displayCtrl', function ($scope, $http, $location, User, $timeout) {
+
+        $scope.image1 = true;
+        $scope.image2 = false;
+        $scope.image3 = false;
+        $scope.image4 = false;
+        $scope.image5 = false;
+        $scope.image6 = false;
+        $scope.image7 = false;
+        $scope.image8 = false;
+        $scope.image9 = false;
+        $scope.image10 = false;
+        $scope.changeImage = function () {
+
+            if ($scope.image1) {
+                  $scope.image1 = false;
+            $scope.image2 = true;
+            $scope.image3 = false;
+            $scope.image4 = false;
+            $scope.image5 = false;
+            $scope.image6 = false;
+            $scope.image7 = false;
+            $scope.image8 = false;
+            $scope.image9 = false;
+            $scope.image10 = false;
+
+            } else if ($scope.image2) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = true;
+                $scope.image4 = false;
+                $scope.image5 = false;
+                $scope.image6 = false;
+                $scope.image7 = false;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+            else if ($scope.image3) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = true;
+                $scope.image5 = false;
+                $scope.image6 = false;
+                $scope.image7 = false;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+            else if ($scope.image4) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = false;
+                $scope.image5 = true;
+                $scope.image6 = false;
+                $scope.image7 = false;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+            else if ($scope.image5) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = false;
+                $scope.image5 = false;
+                $scope.image6 = true;
+                $scope.image7 = false;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+            else if ($scope.image6) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = false;
+                $scope.image5 = false;
+                $scope.image6 = false;
+                $scope.image7 = true;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+            else if ($scope.image7) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = false;
+                $scope.image5 = false;
+                $scope.image6 = false;
+                $scope.image7 = false;
+                $scope.image8 = true;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+            else if ($scope.image9) {
+                $scope.image1 = false;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = false;
+                $scope.image5 = false;
+                $scope.image6 = false;
+                $scope.image7 = false;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = true;
+            }
+            else if ($scope.image10) {
+                $scope.image1 = true;
+                $scope.image2 = false;
+                $scope.image3 = false;
+                $scope.image4 = false;
+                $scope.image5 = false;
+                $scope.image6 = false;
+                $scope.image7 = false;
+                $scope.image8 = false;
+                $scope.image9 = false;
+                $scope.image10 = false;
+            }
+
+
+        }
+
+
+
+    })
+
+}());
